dbc1061cc82c893c72c6d52c96f1e0e9298943c7
Thi Thuy Oanh Nguyen <thuyoanh199930@gmail.com>
2023-04-26 17:11:31 +0000

    complete product info and category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @categories = Category.order(id: :desc).all
+    @count = @categories.length
+    @products = Product.order(id: :desc).all
   end
 end
